8a2426e2263d8de9a2d618307b7545f2f36c54df
nassersaazi <nassersnoop@gmail.com>
2021-02-08 23:50:27 +0300

    .NET Core REST Api initial commit

---
+++ Commander/Models/Command.cs
@@ -0,0 +1,7 @@
+namespace Commander.Models
+{
+    public int Id { get; set; }
+    public string HowTo { get; set; }
+    public string Line { get; set; }
+    public string Platform { get; set; }
+}
