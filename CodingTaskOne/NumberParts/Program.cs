51d8344619304e6f14ae2c19a4117c0b75838ccb
nassersaazi <nassersnoop@gmail.com>
2021-03-27 09:58:47 +0300

    Tested some logic in program file

--- CodingTaskOne/NumberParts/Program.cs
+++ CodingTaskOne/NumberParts/Program.cs
@@ -6,10 +6,32 @@ namespace NumberParts
     {
         static void Main(string[] args)
         {
-
             
-            //Console.WriteLine("Code wars first task!");
+            Program p = new Program();
+            var myArray = p.Splitter(9,4);
+
+            Console.WriteLine("Code wars first task!\n\n" 
+            + "[{0}]", string.Join(", ", myArray));
         }
+
+        public int[] Splitter(int number, int parts){
+                
+                int[] partsArray = new int[parts];
+                int c1 = number - (number % parts);
+                for (int i = 0; i < partsArray.Length; i++)
+                {
+                    if (i == partsArray.Length - 1)
+                    {
+                        partsArray[i] = number % parts;
+                    }
+                    else
+                    {
+                        partsArray[i] = c1 / parts;
+                    }
+                    
+                }
+                return partsArray;
+            }
             // Processor proc = new Processor();
             // proc.DoWork(11,3);
        
